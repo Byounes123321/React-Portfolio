25e6c8677c4e7f40324d81144d20784e11520e4d
Bassil Younes <bassilyounes@gmail.com>
2023-05-02 14:10:22 -0400

    fixed header functonality

--- portfolio/src/Components/Header.jsx
+++ portfolio/src/Components/Header.jsx
@@ -1,9 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import Logo from './imgs/Logo.png';
 import Nav from './Nav';
 
 function Header() {
+  const [isSticky, setIsSticky] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  useEffect(() => {
+    function handleScroll() {
+      const currentScrollPos = window.pageYOffset;
+      if (prevScrollPos > currentScrollPos) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+      setPrevScrollPos(currentScrollPos);
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos]);
+
   return (
-    <header className="header sticky">
+    <header className={isSticky ? 'header sticky' : 'header'}>
       <div className="logo">
         <a href="/">
             <img src={Logo} title="logo" alt="logo for bassilyounes"/>
