3898d0c16f1983301b320fda95ee5350edb4a137
Bassil Younes <bassilyounes@gmail.com>
2023-05-02 15:56:53 -0400

    added socals sidebar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -13,8 +13,8 @@ function App() {
   return (
     <>
     <Header />
+    <Socials/>
     <main className='main'>
-      <Socials/>
       <Landing/>
       <About />
       <Projects />
--- portfolio/src/Components/Socials.jsx
+++ portfolio/src/Components/Socials.jsx
@@ -4,10 +4,12 @@ import github from "./imgs/github.svg";
 
 export default function Socials(){
     return(
+        <aside>
         <div className="socials">
             <a href="https://www.instagram.com/bazazel/?hl=en" target="_blank" rel="noreferrer"><img className="social" src={Insta} alt="Instagram"/></a>
             <a href="https://github.com/Byounes123321"target="_blank" rel="noreferrer"><img className="social" src={github} alt="GitHub"/></a>
             <a href="https://www.linkedin.com/in/bassilyounes/"target="_blank" rel="noreferrer"><img className="social" src={linkedin} alt="LinkedIn"/></a>
         </div>
+        </aside>
      );
 }
