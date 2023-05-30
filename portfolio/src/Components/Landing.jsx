7f570a402b76bd0282eaa28dfe974fac5e16ef4d
Bassil Younes <83778381+Byounes123321@users.noreply.github.com>
2023-05-30 15:21:03 -0400

    edited intro

--- portfolio/src/Components/Landing.jsx
+++ portfolio/src/Components/Landing.jsx
@@ -1,17 +1,20 @@
-export default function Landing(){
-    return(
-        <div class="landing">
-        <p class="hi">Hi, my name is</p>
-        <h1>Bassil Younes</h1>
-        <h2>Full Stack Web Developer</h2>
-        <p>
-          Welcome to my portfolio website! I'm a post-graduate student in the
-          Humber Web Development program with a passion for web development. I
-          specialize in building web-based applications, with occasional design
-          work. My goal is to turn my ideas into reality and create exceptional
-          user experiences. I'm excited to continue learning and exploring the
-          endless possibilities of the web development industry.
-        </p>
-      </div>
-    )
+export default function Landing() {
+  return (
+    <div class="landing">
+      <p class="hi">Hi, my name is</p>
+      <h1>Bassil Younes</h1>
+      <h2>Full Stack Web Developer</h2>
+      <p>
+        Welcome to my portfolio website! As a post-graduate student in the
+        Humber Web Development program, I am driven by a deep passion for web
+        development. I have honed my skills in building web-based applications
+        and occasionally delving into design work. My primary objective is to
+        transform my ideas into tangible solutions and deliver exceptional user
+        experiences. The ever-evolving nature of the web development industry
+        excites me, as it offers endless possibilities for innovation and
+        exploration. I am enthusiastic about continuously learning and pushing
+        the boundaries of what is achievable in this dynamic field.
+      </p>
+    </div>
+  );
 }
