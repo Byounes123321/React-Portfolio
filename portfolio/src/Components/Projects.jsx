369bb22f21409627b7d91545404658f65102068e
Bassil Younes <bassilyounes@gmail.com>
2023-05-31 11:09:19 -0400

    Changed order

--- portfolio/src/Components/Projects.jsx
+++ portfolio/src/Components/Projects.jsx
@@ -37,55 +37,57 @@ export default function Projects() {
 
       {projects ? (
         <>
-          {projects.map((project) => {
-            return (
-              <div className="projects">
-                <div className="projImgs">
-                  <a
-                    href={project.url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="projimga"
-                  >
-                    <img className="projectimg" src={project.image} alt="" />
-                    {/* <img className="gitimg" src="" alt="" /> */}
-                  </a>
-                  <div className="projectSkills">
-                    {project.skills.map((skill) => (
-                      <>
-                        <a
-                          href={skill.url}
-                          target="_blank"
-                          rel="noreferrer"
-                          title={skill.name}
-                        >
-                          <img
-                            src={`https://console.bassilyounes.com/storage/${skill.logo}`}
-                            alt={skill.name}
-                          />
-                        </a>
-                      </>
-                    ))}
+          {projects
+            .sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
+            .map((project) => {
+              return (
+                <div className="projects">
+                  <div className="projImgs">
+                    <a
+                      href={project.url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="projimga"
+                    >
+                      <img className="projectimg" src={project.image} alt="" />
+                      {/* <img className="gitimg" src="" alt="" /> */}
+                    </a>
+                    <div className="projectSkills">
+                      {project.skills.map((skill) => (
+                        <>
+                          <a
+                            href={skill.url}
+                            target="_blank"
+                            rel="noreferrer"
+                            title={skill.name}
+                          >
+                            <img
+                              src={`https://console.bassilyounes.com/storage/${skill.logo}`}
+                              alt={skill.name}
+                            />
+                          </a>
+                        </>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="projDesc">
+                    <a
+                      href={github[project.title]}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="hover-container"
+                    >
+                      <div>
+                        <h3>{project.title}</h3>
+                      </div>
+                      <div>
+                        <p>{project.content}</p>
+                      </div>
+                    </a>
                   </div>
                 </div>
-                <div className="projDesc">
-                  <a
-                    href={github[project.title]}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="hover-container"
-                  >
-                    <div>
-                      <h3>{project.title}</h3>
-                    </div>
-                    <div>
-                      <p>{project.content}</p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </>
       ) : (
         <p>Loading projects...</p>
