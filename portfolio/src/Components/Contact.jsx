89067849c090d1a37837120eae2c047df91efdee
Bassil Younes <bassilyounes@gmail.com>
2023-05-31 11:13:28 -0400

    changed contact me wording

--- portfolio/src/Components/Contact.jsx
+++ portfolio/src/Components/Contact.jsx
@@ -1,25 +1,24 @@
-export default function Contact(){
-
-    return(
-        <div id="contact">
-        <h2>Contact Me</h2>
-        <p>
-          Hello, thank you for visiting my website! If you're interested in
-          learning more about my work or have any questions, feel free to reach
-          out to me using the form below. I'm always looking for exciting new
-          projects to work on and opportunities to learn from experienced
-          professionals in the tech industry. Whether you're interested in
-          discussing a potential placement for my work term starting in July
-          2023, or just want to say hi, I'd love to hear from you! So go ahead
-          and drop me a message, and I'll get back to you as soon as I can.
-        </p>
-        <div class="showMorebtn">
-          <a href="mailto:bassilyounes@gmail.com">
-            <div class="btn">
-              <div class="text">Email me!</div>
-            </div>
-          </a>
-        </div>
+export default function Contact() {
+  return (
+    <div id="contact">
+      <h2>Contact Me</h2>
+      <p>
+        Hello, thank you for visiting my website! I'm always looking for
+        exciting new projects to work on and opportunities to learn from
+        experienced professionals in the tech industry. If you're interested in
+        learning more about my work or have any questions, feel free to reach
+        out to me below. Whether you're interested in discussing a potential
+        placement for my work term starting in July 2023, or just want to say
+        hi, I'd love to hear from you! So go ahead and drop me a message, and
+        I'll get back to you as soon as I can.
+      </p>
+      <div class="showMorebtn">
+        <a href="mailto:bassilyounes@gmail.com">
+          <div class="btn">
+            <div class="text">Email me!</div>
+          </div>
+        </a>
       </div>
-    )
+    </div>
+  );
 }
