0fb55b473e482f61f6d1d4aa989871e3971b8669
Bassil Younes <83778381+Byounes123321@users.noreply.github.com>
2023-08-08 16:34:06 -0400

    updated

--- portfolio/src/Components/Experience.jsx
+++ portfolio/src/Components/Experience.jsx
@@ -8,15 +8,16 @@ export default function Experience() {
         <img src={companyLogo} alt="Company Logo" />
         <div className="eduDesc">
           <div className="school">BlueBird IT Solutions</div>
-          <div className="degree">Software Developer (Summer internship)</div>
-          <div className="time">Start: July</div>
-          <div className="time">End: August </div>
+          <div className="degree">Software Developer (Summer Internship)</div>
+          <div className="time">Start: July 2023</div>
+          <div className="time">End: August 2023</div>
           <div className="schoolDesc">
             Participated in the development of an internal web application
-            designed to manage customer billings and payments. Some of the tasks
-            I was assigned to do were: Implement API's, Create components, Write
-            Documentation, and write unit tests. Technologies used: Next.js,
-            SQL, TypeScript, AntD, Jest, Docker, Java, Spring Boot, and Git.
+            designed to manage customer billing and payments. Some of the tasks
+            I was assigned to do were: Implement and interact with API's, create
+            components, write documentation, and write unit tests. Technologies
+            used: Next.js, SQL, TypeScript, AntD, Jest, Docker, Java, Spring
+            Boot, and Git.
           </div>
         </div>
       </div>
